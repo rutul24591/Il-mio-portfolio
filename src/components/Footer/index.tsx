783b4b23730cdda9efe8b36649dcc39fbf02667c
Rutul Amin <rutulamin.ra@gmail.com>
2022-06-10 12:21:01 +0530

    Added Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,26 +4,66 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faLinkedin,
     faGithub,
-    faYoutube,
     faSkype,
+    faMedium,
+    faTwitter,
 } from '@fortawesome/free-brands-svg-icons';
-// import { faHome, faUser, faEnvelope, faSuitcase } from '@fortawesome/free-solid-svg-icons';
 
 import './index.scss';
 
 const Footer = () => {
     return(
-        // <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
         <Container fluid className="footer-container">
             <Row>
                 <Col md="4" className="footer-social">
-                    <h4>Copyright @2022 Rutul Amin</h4>
+                    <ul>
+                        <li>
+                            <a 
+                                href="https://www.google.com"
+                                target="_blank"
+                                rel="noreferrer"
+                            />
+                            <FontAwesomeIcon icon={faLinkedin} color="#FFFFFF" className="fa-linkedin fa-xl"/>
+                        </li>
+                        <li>
+                            <a 
+                                href="https://www.google.com"
+                                target="_blank"
+                                rel="noreferrer"
+                            />
+                            <FontAwesomeIcon icon={faMedium} color="#FFFFFF" className="fa-medium fa-xl"/>
+                        </li>
+                        <li>
+                            <a 
+                                href="https://www.google.com"
+                                target="_blank"
+                                rel="noreferrer"
+                            />
+                            <FontAwesomeIcon icon={faSkype} color="#FFFFFF" className="fa-skype fa-xl"/>
+                        </li>
+                        <li>
+                            <a 
+                                href="https://www.google.com"
+                                target="_blank"
+                                rel="noreferrer"
+                            />
+                            <FontAwesomeIcon icon={faGithub} color="#FFFFFF" className="fa-github fa-xl"/>
+                        </li>
+                        <li>
+                            <a 
+                                href="https://www.google.com"
+                                target="_blank"
+                                rel="noreferrer"
+                            />
+                            <FontAwesomeIcon icon={faTwitter} color="#FFFFFF" className="fa-twitter fa-xl"/>
+                        </li>
+                    </ul>
                 </Col>
                 <Col md="4" className="footer-body">
-                    <h4>Copyright @2022 Rutul Amin</h4>
+                    <h6>Designed and Developed by <i>Rutul Amin</i></h6>
                 </Col>
                 <Col md="4" className="footer-copyrights">
-                    <h4>Copyright @2022 Rutul Amin</h4>
+                    <h6>Copyright @2022 Rutul Amin</h6>
                 </Col>
             </Row>
         </Container>
