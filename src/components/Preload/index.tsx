9756d50ac620db9df91d9005792950399acb4619
Rutul Amin <rutulamin.ra@gmail.com>
2025-07-24 15:49:13 +0530

    removing old cold

--- src/components/Preload/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const Preload = (props: any) => {
-  return <div id={props.loader ? "preloader" : "no-preloader"}></div>;
-};
-
-export default Preload;
