08c1dae9370ba5320964569ec8be0110d7647174
rutul24591 <rutulamin.ra@gmail.com>
2025-12-30 08:14:33 +0530

    Lazy loading About, workex and resume component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, lazy, Suspense } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { preload } from "react-dom";
@@ -10,9 +10,13 @@ import Loader from "./components/Preload";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { ScrollToTop } from "./helper";
 import Home from "./components/Home";
-import About from "./components/About";
-import WorkEx from "./components/WorkEx";
-import Resume from "./components/Resume";
+// import About from "./components/About";
+// import WorkEx from "./components/WorkEx";
+// import Resume from "./components/Resume";
+
+const AboutComponent = lazy(() => import("./components/About"));
+const WorkExComponent = lazy(() => import("./components/WorkEx"));
+const ResumeComponent = lazy(() => import("./components/Resume"));
 
 const App = () => {
   console.log("DEBUG: App component rendered");
@@ -38,9 +42,30 @@ const App = () => {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/workex" element={<WorkEx />} />
-          <Route path="/resume" element={<Resume />} />
+          <Route
+            path="/about"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <AboutComponent />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/workex"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <WorkExComponent />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/resume"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <ResumeComponent />
+              </Suspense>
+            }
+          />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
